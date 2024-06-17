24af58fa9fa2af4a2b39774615a42da0e81f385b
KKmanuu <kipngenoemmanuel479@gmail.com>
2024-06-17 15:36:19 +0300

    feat: Add project list with detailed descriptions and tech stacks

--- src/db/projectdb.js
+++ src/db/projectdb.js
@@ -28,7 +28,7 @@ export const ProjectList = [
     title: "TODO-LISTS",
     description:
       "Efficient JavaScript To-Do List with Status Updates: An enhanced version featuring task management, status updates, and local storage persistence. Users can add, delete, and edit tasks, mark them as completed/incomplete with checkboxes, and easily clear completed tasks. ",
-    techStack: [nextjs, tailwind, html, css],
+    techStack: [nextjs, tailwind, html, css,javascript],
     liveLink: "https://kkmanuu.github.io/To-Do-Lists/dist/",
     gitLink: "https://github.com/kkmanuu/To-Do-Lists",
   },
@@ -38,7 +38,7 @@ export const ProjectList = [
     title: "CAR-BOOKING-WEBSITE",
     description:
       " An application programming interface (API) tailored for car rental services streamlines the integration of rental functionalities into various applications or systems. It empowers users to execute essential tasks associated with vehicle rental, including reservation and inventory management.",
-    techStack: [node, Rails, Ruby, react, redux, html , tailwind, css, Javascript],
+    techStack: [node, Rails, Ruby, react, redux, html , tailwind, css, javascript],
     liveLink: "https://wheels-4-rentv1.onrender.com/",
     gitLink: "https://github.com/kkmanuu/Wheels-4-Rent-backend",
     gitLink: "https://github.com/kkmanuu/Wheels-4-Rent-frontend",
@@ -49,9 +49,9 @@ export const ProjectList = [
     title: " CRYPTO",
     description:
       "CryptoSphere is a sophisticated platform for users to access comprehensive information regarding the leading 30 cryptocurrencies.",
-    techStack: [nextjs, tailwind, html, css],
-    liveLink: "",
-    gitLink: "",
+    techStack: [nextjs, tailwind, html, css, javascript],
+    liveLink: "https://cryptocurrency-agir.onrender.com/",
+    gitLink: "https://github.com/kkmanuu/cryptocurrency",
   },
   {
     images: 'bookstore.png',
@@ -60,7 +60,7 @@ export const ProjectList = [
     details: ['Microverse', 'Front-end', '2023'],
     description:
       ' This project lay the foundations for a Bookstore website. It create the user interface using React.',
-    techStack: [nextjs, tailwind, html, css],
+    techStack: [nextjs, tailwind, html, css,javascript],
     liveLink: 'https://bookstore-bg5v.onrender.com/',
     sourceLink: 'https://github.com/kkmanuu/bookstore',
   },
@@ -70,72 +70,39 @@ export const ProjectList = [
     title: "LEADERBOARD",
     description:
       "The leaderboard website displays scores submitted by different players and It also allows you to submit your score.",
-    techStack: [nextjs, tailwind, html, css],
+    techStack: [nextjs, tailwind, html, css,javascript],
     liveLink: "https://kkmanuu.github.io/Leaderboard/dist/",
     gitLink: "https://github.com/kkmanuu/Leaderboard",
   },
   {
-    heading: "JOKE GENERATOR",
-    img: "",
-    title: "",
+    heading: "MULTILINGUAL-WEATHER-DASHOARD",
+    images: "ApI.png.jpeg",
+    title: "multilingual-weather-dashboard",
     description:
-      "",
-    techStack: [react,typescript,node,promptEngineering,express],
-    liveLink: "",
-    gitLink: "",
+      "This is a straightforward weather application constructed with React that enables users to view the current weather and forecast for a specific location using the OpenWeatherMap API.",
+    techStack: [react,typescript,node,javascript,],
+    liveLink: "https://multilingual-weather-dashboard.onrender.com/",
+    gitLink: "https://github.com/kkmanuu/multilingual-weather-dashboard",
   },
   {
-    heading: "Code-Ninja",
-    img: "",
-    title: "Code Converter, Code Debugger",
+    heading: "GlOBAL SUMMIT",
+    images: "Globol.png.jpeg",
+    title: "global summit ",
     description:
-      "",
-    techStack: [react, typescript, chakra, promptEngineering, node],
-    liveLink: "https://code-converter-brown.vercel.app/",
-    gitLink: "https://github.com/SumatM/Code-Ninja",
+      "This is my first capstone project in microverse About Creative Commons Global Summit for 2015.",
+    techStack: [ typescript, html, css, node,javascript],
+    liveLink: "https://kkmanuu.github.io/Capstone-1/",
+    gitLink: "https://github.com/kkmanuu/Capstone-1",
   },
   {
-    heading: "E.M.S",
-    img: "",
-    title: "",
+    heading: "MATH-MAGICIANS",
+    images: "Math.png.jpeg",
+    title: "math-magicians",
     description:
-      "",
-    techStack: [react, typescript, chakra, redux, node, express, mongodb],
-    liveLink: "",
+      "Math magicians This a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations. Read a random math-related quote.",
+    techStack: [html, typescript, html, node,javascript],
+    liveLink: "https://math-magicians-u8mn.onrender.com/",
     gitLink:
-      "",
-  },
-  {
-    heading: "Aroma",
-    img: "",
-    title: "",
-    description: " ",
-    techStack: [react, tailwind, redux, node, mongodb, express],
-    liveLink: "",
-    gitLink: "",
-  },
-];
-
-const extra = [
-  {
-    heading: "",
-    img: "",
-    title: "",
-    description:
-      "",
-    techStack: [html, javascript, css],
-    liveLink: "",
-    gitLink: "",
-  },
-  ,
-  {
-    heading: "SPENDWISER",
-    img: "",
-    title: "",
-    description:
-      "",
-    techStack: [react, chakra, redux, chartjs],
-    liveLink: "",
-    gitLink: "",
+      "https://github.com/kkmanuu/math-magicians",
   },
 ];
