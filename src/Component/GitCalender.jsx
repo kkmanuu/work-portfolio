8a6df6b4f41dbf5b306c0e3a8e9cda3fd7e0d51e
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-01-17 10:53:31 +0300

    feat: Add GitHub stats and calendar integration

--- src/Component/GitCalender.jsx
+++ src/Component/GitCalender.jsx
@@ -12,14 +12,14 @@ export default function GitCalender(){
             </Flex>
             <Flex  mt='40px' flexDirection={{base:'column',sm:'column',md:'column',lg:'row'}} justifyContent='space-around' alignItems='center' gap='25px'>
                 <Box >
-                    <img display="block" id="github-stats-card" src='https://camo.githubusercontent.com/98fef3cdf1922b3c8912446be97825d6e01d1ce3a5ff3d4c003351fe046eb590/68747470733a2f2f6769746875622d726561646d652d73746174732e76657263656c2e6170702f6170693f757365726e616d653d6b6b6d616e75752673686f775f69636f6e733d74727565266c6f63616c653d656e' alt="Github Staus Card"/>
+                    <img display="block" id="github-status-card" src='https://camo.githubusercontent.com/05431107f731eb99ccd990c803f7d6f2b5297f6d912a91a13bdec034a8f8a68f/68747470733a2f2f6769746875622d726561646d652d73746174732d6d617576652d74656e2e76657263656c2e6170702f6170693f757365726e616d653d6b6b6d616e75752673686f775f69636f6e733d7472756526686964655f626f726465723d7472756526636f756e745f707269766174653d7472756526696e636c7564655f616c6c5f636f6d6d6974733d74727565' alt="Github Staus Card"/>
                 </Box>
                 <Box >
-            <img style={{display:'block'}} id="github-streak-stats"  src="https://camo.githubusercontent.com/545eb1da63523de893be37ccffe82aef4e2481825f42e9d67042421dc0c3ebc5/68747470733a2f2f6769746875622d726561646d652d73747265616b2d73746174732e6865726f6b756170702e636f6d2f3f757365723d6b6b6d616e757526" alt="Github streak stats"/>
+            <img style={{display:'block'}} id="github-Trophies"  src="https://camo.githubusercontent.com/5c22fc7118d7fe06cb49c28dbfea1bada4e9f29ba323df456a844d54a4272285/68747470733a2f2f6769746875622d70726f66696c652d74726f7068792e76657263656c2e6170702f3f757365726e616d653d6b6b6d616e757526636f6c756d6e3d3826726f773d31" alt="Github streak stats"/>
             </Box>
             </Flex> 
             <Flex  mt='40px'  justifyContent='center'>
-              <img display="block" id="github-top-langs" src="https://camo.githubusercontent.com/f6d7d8f2b3adceb6461d6223d1195b3f192eacef6ba2a1fbeea797b42390d1b4/68747470733a2f2f6769746875622d726561646d652d73746174732e76657263656c2e6170702f6170692f746f702d6c616e67733f757365726e616d653d6b6b6d616e75752673686f775f69636f6e733d74727565266c6f63616c653d656e266c61796f75743d636f6d70616374"/>
+              <img display="block" id="github-graphs" src="https://camo.githubusercontent.com/7d1bb6c3433efd6f2891dc2474327297112eb69971220cf364c679d1f9f26603/68747470733a2f2f6769746875622d726561646d652d61637469766974792d67726170682e76657263656c2e6170702f67726170683f757365726e616d653d6b6b6d616e7575267468656d653d6d696e696d616c"/>
                 </Flex>
             </Box>
         </Box>
